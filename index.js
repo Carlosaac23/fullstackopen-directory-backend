77ad3931dc9b02b85bfef0cfb7cf5488c481b932
Carlos <carlosaac232001@gmail.com>
2026-06-18 09:48:28 -0500

    add env var for PORT

--- index.js
+++ index.js
@@ -88,6 +88,6 @@ app.delete("/api/contacts/:id", (req, res) => {
   return res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
