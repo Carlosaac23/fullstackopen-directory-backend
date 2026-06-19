68e368f3aacf8e7ebce959b0c6b61bd37fdaa043
Carlos <carlosaac232001@gmail.com>
2026-06-19 11:44:16 -0500

    solve exercise 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Contact = require("./models/contact");
 
 const app = express();
 
@@ -10,76 +11,62 @@ app.use(cors());
 morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
-let contacts = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    phone: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    phone: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    phone: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    phone: "39-23-6423122",
-  },
-];
+// let contacts = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     phone: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     phone: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     phone: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     phone: "39-23-6423122",
+//   },
+// ];
 
-function generateId() {
-  const maxId = contacts.length > 0 ? Math.max(...contacts.map((contact) => contact.id)) : 0;
-  return maxId + 1;
-}
+// function generateId() {
+//   const maxId = contacts.length > 0 ? Math.max(...contacts.map((contact) => contact.id)) : 0;
+//   return maxId + 1;
+// }
 
 app.post("/api/contacts", (req, res) => {
   const body = req.body;
-  console.log("body", body);
-
-  const contactExists = contacts.find((contact) => contact.name === body.name);
 
   if (!body.name || !body.phone) {
     return res.status(400).json({ error: "name or phone missing" });
   }
 
-  if (contactExists) {
-    return res.status(409).json({ error: "name must be unique" });
-  }
-
-  const contact = {
-    id: generateId(),
+  const contact = new Contact({
     name: body.name,
     phone: body.phone,
-  };
+  });
 
-  contacts.concat(contact);
-
-  return res.json(contact);
+  contact.save().then((savedContact) => res.json(savedContact));
 });
 
 app.get("/api/contacts", (req, res) => {
-  return res.json(contacts);
+  Contact.find({}).then((contacts) => res.json(contacts));
 });
 
 app.get("/info", (req, res) => {
-  res.send(`<p>Phonebook has info for ${contacts.length} people</p> <p>${new Date()}</p>`);
+  Contact.find({}).then((contacts) => {
+    res.send(`<p>Phonebook has info for ${contacts.length} people</p> <p>${new Date()}</p>`);
+  });
 });
 
 app.get("/api/contacts/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const contact = contacts.find((contact) => contact.id === id);
-
-  if (!contact) {
-    return res.status(404).json({ error: "contact not found" });
-  }
-
-  return res.json(contact);
+  const id = req.params.id;
+  Contact.findById(id).then((contact) => res.json(contact));
 });
 
 app.delete("/api/contacts/:id", (req, res) => {
@@ -89,6 +76,6 @@ app.delete("/api/contacts/:id", (req, res) => {
   return res.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
